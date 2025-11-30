0fe5c6abe4b54f1f0eead042a0e6234df81320b7
nikhil25nik <nikhilkurane25@gmail.com>
2025-11-30 11:57:12 +0530

    second

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,69 +1,93 @@
+import "./App.css";
 
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { RouteAddBlog, RouteAddCategory, RouteBlog, RouteBlogByCategory, RouteBlogDetails, RouteCategoryDetails, RouteCommentDetails,  RouteEditBlog, RouteEditCategory, RouteIndex, RouteProfile, RouteSearch, RouteSignIn, RouteSignUp, RouteUsers } from './helper/RouteName'
-import Index from './pages'
-import Layout from './layout/Layout'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
-import AddCategory from './pages/Categories/AddCategory'
-import EditCategory from './pages/Categories/EditCategory'
-import CategoryDetails from './pages/Categories/CategoryDetails'
-import AddBlog from './pages/Blog/AddBlog'
-import BlogDetails from './pages/Blog/BlogDetails'
-import EditBlog from './pages/Blog/EditBlog'
-import SingleBlogDetail from './pages/singleBlogDetail'
-import BlogByCategory from './components/BlogByCategory'
-import SearchResult from './components/SearchResult'
-import CommentDetails from './pages/CommentDetails'
-import Users from './pages/Users'
-import AuthRouteProtection from './components/AuthRouteProtection'
-import AllowedAdminOnly from './components/AllowedAdminOnly'
+import {
+  RouteAddBlog,
+  RouteAddCategory,
+  RouteBlog,
+  RouteBlogByCategory,
+  RouteBlogDetails,
+  RouteCategoryDetails,
+  RouteCommentDetails,
+  RouteEditBlog,
+  RouteEditCategory,
+  RouteIndex,
+  RouteProfile,
+  RouteSearch,
+  RouteSignIn,
+  RouteSignUp,
+  RouteUsers,
+} from "./helper/RouteName";
+
+
+import Index from "./pages/Index.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Profile from "./pages/Profile.jsx";
+
+
+import AddCategory from "./pages/Categories/AddCategory.jsx";
+import EditCategory from "./pages/Categories/EditCategory.jsx";
+import CategoryDetails from "./pages/Categories/CategoryDetails.jsx";
+
+
+import AddBlog from "./pages/Blog/AddBlog.jsx";
+import BlogDetails from "./pages/Blog/BlogDetails.jsx";
+import EditBlog from "./pages/Blog/EditBlog.jsx";
+import SingleBlogDetail from "./pages/SingleBlogDetail.jsx"; 
+
+
+
+import BlogByCategory from "./components/BlogByCategory.jsx";
+import SearchResult from "./components/SearchResult.jsx";
+import AuthRouteProtection from "./components/AuthRouteProtection.jsx";
+import AllowedAdminOnly from "./components/AllowedAdminOnly.jsx";
+
+import CommentDetails from "./pages/CommentDetails.jsx";
+import Users from "./pages/Users.jsx";
+
+
+import Layout from "./layout/Layout.jsx";
 
 function App() {
-
-
   return (
     <>
-     <BrowserRouter>
-     <Routes>
-      <Route path={RouteIndex} element={<Layout/>}>
-      <Route index element={<Index/>}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path={RouteIndex} element={<Layout />}>
+            <Route index element={<Index />} />
+
+    
+            <Route path={RouteBlogDetails()} element={<SingleBlogDetail />} />
+            <Route path={RouteBlogByCategory()} element={<BlogByCategory />} />
+            <Route path={RouteSearch()} element={<SearchResult />} />
+
+    
+            <Route element={<AuthRouteProtection />}>
+              <Route path={RouteAddBlog} element={<AddBlog />} />
+              <Route path={RouteEditBlog()} element={<EditBlog />} />
+              <Route path={RouteBlog} element={<BlogDetails />} />
+              <Route path={RouteCommentDetails} element={<CommentDetails />} />
+              <Route path={RouteProfile} element={<Profile />} />
+            </Route>
 
       
-      
-      <Route path={RouteBlogDetails()} element={<SingleBlogDetail/>}/>
-      <Route path={RouteBlogByCategory()} element={<BlogByCategory/>}/>
-      <Route path={RouteSearch()} element={<SearchResult/>}/>
+            <Route element={<AllowedAdminOnly />}>
+              <Route path={RouteUsers} element={<Users />} />
+              <Route path={RouteEditCategory()} element={<EditCategory />} />
+              <Route path={RouteCategoryDetails} element={<CategoryDetails />} />
+              <Route path={RouteAddCategory} element={<AddCategory />} />
+            </Route>
+          </Route>
 
-
-      <Route element={<AuthRouteProtection/>}>
-      <Route path={RouteAddBlog} element={<AddBlog/>}/>
-       <Route path={RouteEditBlog()} element={<EditBlog/>}/>
-      <Route path={RouteBlog} element={<BlogDetails/>}/>
-      <Route path={RouteCommentDetails} element={<CommentDetails/>}/>
-      <Route path={RouteProfile} element={<Profile/>}/>
-      </Route>
-
-      <Route element={<AllowedAdminOnly/>}>
-         <Route path={RouteUsers} element={<Users/>}/>
-          <Route path={RouteEditCategory()} element={<EditCategory/>}/>
-      <Route path={RouteCategoryDetails} element={<CategoryDetails/>}/>
-      <Route path={RouteAddCategory } element={<AddCategory/>}/>
-       <Route path={RouteUsers} element={<Users/>}/>
-      </Route>
-
-      </Route>
-
-
-      <Route path={RouteSignIn} element={<SignIn/>}/>
-      <Route path={RouteSignUp} element={<SignUp/>}/>
-     </Routes>
-     </BrowserRouter>
+    
+          <Route path={RouteSignIn} element={<SignIn />} />
+          <Route path={RouteSignUp} element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
