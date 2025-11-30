2af7f2251bd535d06d83cbfa4bc36fad782f5e1c
nikhil25nik <nikhilkurane25@gmail.com>
2025-11-30 17:41:09 +0530

    changes in index

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+
 import AuthRoute from "./routes/auth.routes.js";
 import { errorMiddleware } from "./helpers/handelError.js";
 import UserRoute from "./routes/user.routes.js";
@@ -11,51 +12,64 @@ import BlogRoute from "./routes/blog.routes.js";
 import CommentRoute from "./routes/comment.routes.js";
 import BlogLikeRoute from "./routes/blogLike.route.js";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cookieParser());
-app.use(express.json());
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
-}))
-
-app.use("/api/auth",AuthRoute);
-app.use("/api/user",UserRoute);
-app.use("/api/category",CategoryRoute)
-app.use("/api/blog",BlogRoute)
-app.use("/api/comment",CommentRoute)
-app.use("/api/like",BlogLikeRoute)
-
-main()
-.then(()=>{
-    console.log("DB Connect successfully");
-})
-.catch((err)=>{
-    console.log(err);
-})
-async function main(){
-await mongoose.connect(process.env.MONGO_CONN)
-}
 
 
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL, 
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true
+    })
+);
+
+// Routes
+app.use("/api/auth", AuthRoute);
+app.use("/api/user", UserRoute);
+app.use("/api/category", CategoryRoute);
+app.use("/api/blog", BlogRoute);
+app.use("/api/comment", CommentRoute);
+app.use("/api/like", BlogLikeRoute);
+
+// DB
+mongoose
+    .connect(process.env.MONGO_CONN)
+    .then(() => console.log("DB Connected"))
+    .catch(console.error);
+
+// Error Handler
 app.use(errorMiddleware);
 
-
-app.listen(PORT,()=>{
-    console.log(`app is listening on port ${PORT}`);
-})
-
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || "internal server error";
+    const message = err.message || "Internal server error";
 
     res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
-        message,
-    })
-})
+        message
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
